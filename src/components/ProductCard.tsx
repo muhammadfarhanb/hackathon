56b6f5c89e28cdd69f08126bced243db16db775c
farhanbashir <farhan2526313@gmail.com>
2023-07-09 15:31:55 +0500

    upload

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -15,7 +15,7 @@ function ProductCard (props : {
   return (
     <div>  
         <div className=' '>
-          <Link href={`AllProduct/${props.id}`}>
+          <Link href={`Product/${props.id}`}>
             <Image src={props.img} alt='Product' className='w-[300px] h-[350px] bg-gray-200'  />
             <h3 className='font-bold text-lg mt-3 '>{props.title}</h3>
             <p  className='font-bold text-lg '>${props.price} </p>
